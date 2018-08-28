73b376fda0b9873b392dd1262dd221ff94c7253e
Umbrellagun <calebrechten@gmail.com>
2018-08-28 18:37:11 -0400

    Fix for webpack build.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,18 +3,32 @@ const path    = require("path");
 
 const PORT = process.env.PORT || 1333;
 
+const DEVELOPMENT = (process.env.NODE_ENV === "dev");
+
+const entry = (DEVELOPMENT) ? ([
+  `webpack-hot-middleware/client?path=http://localhost:${PORT}/__webpack_hmr&timeout=20000`,
+  "./client/js/app.js"
+]) : ({
+  "dist/js/bundle.js": "./scripts/app.js",
+});
+
+const output = (DEVELOPMENT) ? ({
+  path: __dirname,
+  publicPath: "/",
+  filename: "bundle.js"
+}) : ({
+  path: __dirname,
+  publicPath: "/",
+  filename: "[name]"
+});
+
+const mode = (DEVELOPMENT) ? ("development") : ("production");
+
 module.exports = {
   context: __dirname,
-  entry: [
-    `webpack-hot-middleware/client?path=http://localhost:${PORT}/__webpack_hmr&timeout=20000`,
-    "./client/js/app.js"
-  ],
-  mode: "development",
-  output: {
-    path: __dirname,
-    publicPath: "/",
-    filename: "bundle.js"
-  },
+  entry,
+  mode,
+  output,
   module: {
     rules: [
       {
