d01d67d7e73bf9ec9ee13413792f69f1d8224915
Umbrellagun <calebrechten@gmail.com>
2018-09-16 18:41:37 -0400

    Updated version.

--- client/js/HomePage.js
+++ client/js/HomePage.js
@@ -19,7 +19,7 @@ export default class HomePage extends React.Component {
 
   constructor(props){
     super(props);
-    this.currentVersion = 3;
+    this.currentVersion = 4;
     this.state = {
       pokemon: [],
       filters: {
